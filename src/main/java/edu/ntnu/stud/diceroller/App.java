4b6862b07111302b8be4c0d9d2fb26179e7e09a4
kaamyashinde <kaamyashinde@gmail.com>
2024-07-07 20:33:48 +0530

    implement images for the dice

--- src/main/java/edu/ntnu/stud/diceroller/App.java
+++ src/main/java/edu/ntnu/stud/diceroller/App.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 /**
  * The main application class that handles the starting of the javafx application.
- * @version 0.1
+ * @version 0.2
  * @since 0.1
  * @author kaamyashinde
  */
@@ -22,9 +22,9 @@ public class App extends Application {
    */
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/edu/ntnu/stud/diceroller/fxml/app-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
+    stage.setTitle("Dice Roller");
     stage.setScene(scene);
     stage.show();
   }
--- src/main/java/edu/ntnu/stud/diceroller/Controller.java
+++ src/main/java/edu/ntnu/stud/diceroller/Controller.java
@@ -1,24 +1,56 @@
 package edu.ntnu.stud.diceroller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
 
 /**
  * This class acts as the controller for the JavaFX application.
- * @version 0.1
+ * @version 0.2
  * @since 0.1
  * @author kaamyashinde
  */
 
 public class Controller {
   @FXML
-  private Label welcomeText;
+  private Label diceNumberLabel;
+  @FXML
+  private ImageView diceImageView;
+  private final DiceRoller dice = new DiceRoller();
+  private int numRolled;
+
 
   /**
    * Call to action method for the button that displays a welcome text.
    */
   @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  protected void onRollDiceButtonClicked() {
+    try{
+    dice.rollDice();
+    numRolled = dice.getDiceNumber();
+    diceNumberLabel.setText(Integer.toString(numRolled));
+      updateDiceImage();
+    }
+    catch (Exception e){
+      System.getLogger(e.getMessage());
+    }
+  }
+
+  /**
+   * Method that updates the dice image based on the number rolled.
+   * This is done by creating a new image object, setting it to a new image view, and then setting the FXML image view.
+   */
+  @FXML
+  protected void updateDiceImage(){
+    String diceImage = "/edu/ntnu/stud/diceroller/images/" + numRolled + ".png";
+    Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(diceImage)));
+    ImageView imageView = new ImageView(image);
+    imageView.setFitHeight(100);
+    imageView.setFitWidth(100);
+    diceImageView.setImage(image);
   }
 }
